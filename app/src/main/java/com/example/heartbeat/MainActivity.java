cc8d7354a737262b4a5cb25a16c34b303a1cd800
OliverScharer <oliver.scharer@web.de>
2021-01-06 22:41:55 +0100

    Login screens1

--- app/src/main/java/com/example/heartbeat/MainActivity.java
+++ app/src/main/java/com/example/heartbeat/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.telephony.ClosedSubscriberGroupInfo;
 import android.view.View;
 
 import android.view.Menu;
@@ -19,17 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
