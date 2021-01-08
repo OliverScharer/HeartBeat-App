5a8f0053b9546daedbcec85b19ae1b34ba251710
Sxmxx <simon.lukas60@web.de>
2021-01-08 11:42:00 +0100

    Man kann nun von register zu login wechseln und umgekehrt. Außerdem habe ich die class Profil erstellt.

--- app/src/main/java/com/example/heartbeat/login1.java
+++ app/src/main/java/com/example/heartbeat/login1.java
@@ -1,6 +1,11 @@
 package com.example.heartbeat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login1 extends AppCompatActivity
@@ -8,6 +13,23 @@ public class login1 extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login1);
+        TextView wechsellinkTxt = (TextView) findViewById(R.id.wechsellinkTxt);
+        wechsellinkTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login1.this, register.class));
+            }
+        });
+        Button registrierButton = (Button)findViewById(R.id.registrierButton);
+        registrierButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login1.this, profil.class));
+            }
+        });
+
+
+
     }
 
 }
--- app/src/main/java/com/example/heartbeat/register.java
+++ app/src/main/java/com/example/heartbeat/register.java
@@ -1,6 +1,10 @@
 package com.example.heartbeat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,20 @@ public class register extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        TextView wechsellinkTxt = (TextView) findViewById(R.id.wechsellinkTxt);
+        wechsellinkTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(register.this, login1.class));
+            }
+        });
+        Button registrierButton = (Button)findViewById(R.id.registrierButton);
+        registrierButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(register.this, profil.class));
+            }
+        });
+
     }
 }
