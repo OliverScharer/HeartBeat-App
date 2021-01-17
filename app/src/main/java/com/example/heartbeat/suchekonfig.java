69ae75c036b4f0f92c5334a5c21e1cd9544daccd
Sxmxx <simon.lukas60@web.de>
2021-01-17 01:19:08 +0100

    hab mal weiter gemacht

--- app/src/main/java/com/example/heartbeat/suchekonfig.java
+++ app/src/main/java/com/example/heartbeat/suchekonfig.java
@@ -1,6 +1,8 @@
 package com.example.heartbeat;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,14 @@ public class suchekonfig extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.suchekonfig);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sex, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
