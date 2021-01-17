64719427a91dc592a16601e29ddf1e875f20648e
Sxmxx <simon.lukas60@web.de>
2021-01-17 02:21:57 +0100

    meh

--- app/src/main/java/com/example/heartbeat/suchekonfig.java
+++ app/src/main/java/com/example/heartbeat/suchekonfig.java
@@ -26,13 +26,6 @@ public class suchekonfig extends AppCompatActivity
         });
 
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.sex, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+
     }
 }
